c6c115064d00dbbcd072265c46a2b724a7c20ca1
layssa143 <fabriciamanoelle@icloud.com>
2026-08-14 09:51:21 -0300

    adicionando tabela incidente

--- SLNTask/DataBase/DbTask_Inicial.sql
+++ SLNTask/DataBase/DbTask_Inicial.sql
@@ -27,6 +27,15 @@ CREATE TABLE Tarefa (
 );
 GO
 
+CREATE TABLE Incidente (
+    Codigo INT IDENTITY(1,1) PRIMARY KEY,
+    DescricaoProblema VARCHAR(250) NOT NULL,
+    DataIncidente DATETIME NOT NULL,
+    Solucao VARCHAR(250) NULL,
+    Resolvido VARCHAR(3) NOT NULL -- 'sim' ou 'nao'
+);
+GO
+
 CREATE TABLE Departamento (
     Codigo INT IDENTITY(1,1) PRIMARY KEY,
     Descricao VARCHAR(250) NOT NULL,
