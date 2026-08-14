7f58570168d6446f21eebd5522f3b47eb3f019ae
FabriciaM <fabriciamanoelle@icloud.com>
2026-08-14 16:00:35 -0300

    regra tarefa feita

---
+++ SLNTask/AppTask/Models/Interfaces/IRegraTarefa.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace AppTask.Models.Interfaces
+{
+    public interface IRegraTarefa
+    {
+        bool validarDataFinal(DateTime? dataInicial, DateTime? dataFinal);
+    }
+}
